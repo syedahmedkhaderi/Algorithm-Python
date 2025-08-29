6b84e394f34267aba48a2c6e533cfe9984caed67
syedahmedkhaderi <syedahmedkhaderi@gmail.com>
2025-08-29 22:37:56 +0300

    3 Metthod added to insertion

--- Sorting/Insertionsort.py
+++ Sorting/Insertionsort.py
@@ -1,8 +1,50 @@
 #In Insertion sort, for the first iteration we fix the first element, assuming it is at its correct position
 #Then we loop through the rest of the elements and insert them in their correct positions, with respect to the alreay sorted part of the array
 #Time complexity is O(n^2) in worst case
+def insertion_sort(arr):
+    end = arr[0]
+    i = 1
+    count = 0
+    while i < len(arr):
+        count += 1
+        if arr[i] < end:
+            x = arr.pop(i)
+            for j in range(0, i):
+                if x < arr[j]:
+                    arr.insert(j,x)
+                    break
+        end = arr[i]
+        i += 1
+    return f" {arr}, No. of passes: {count}"
 
-def insertion_sort(array):
+array = [5,9,3,10,45,2,0]
+print(insertion_sort(array))
+
+sorted_array = [5,6,7,8,9]
+print(insertion_sort(sorted_array))
+#It is fast for sorted or nearly sorted inputs as can be seen with the number of comparisons above.
+
+reverse_sorted_array = [9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+print(insertion_sort(reverse_sorted_array))
+
+
+# Method 2
+def insertion_sort2(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]  # Current element to insert
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]  # Shift element to the right
+            j -= 1
+        arr[j + 1] = key   # Place key at correct position
+    return arr
+
+# Example usage:
+arr = [11, 9, 3, 10, 45, 2, 0]
+print("Sorted array:", insertion_sort2(arr))
+
+# Method 3
+def insertion_sort3(array):
     count = 0
     for i in range(1, len(array)):
         print(array)
@@ -22,75 +64,3 @@ def insertion_sort(array):
                     array[j+1] = temp #So we just assign the element to its correct position
                     break #And break out of this loop
     return (f'{array} \nNumber of comparisons = {count}')
-
-array = [5,9,3,10,45,2,0]
-print(insertion_sort(array))
-'''
-[5, 9, 3, 10, 45, 2, 0]
-[5, 9, 3, 10, 45, 2, 0]
-[3, 5, 9, 10, 45, 2, 0]
-[3, 5, 9, 10, 45, 2, 0]
-[3, 5, 9, 10, 45, 2, 0]
-[2, 3, 5, 9, 10, 45, 0]
-[0, 2, 3, 5, 9, 10, 45] 
-Number of comparisons = 19
-'''
-
-sorted_array = [5,6,7,8,9]
-print(insertion_sort(sorted_array))
-'''
-[5, 6, 7, 8, 9]
-[5, 6, 7, 8, 9]
-[5, 6, 7, 8, 9]
-[5, 6, 7, 8, 9]
-[5, 6, 7, 8, 9] 
-Number of comparisons = 4
-'''
-
-#It is fast for sorted or nearly sorted inputs as can be seen with the number of comparisons above.
-
-reverse_sorted_array = [9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
-print(insertion_sort(reverse_sorted_array))
-'''
-[9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[8, 9, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[7, 8, 9, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[6, 7, 8, 9, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[5, 6, 7, 8, 9, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[4, 5, 6, 7, 8, 9, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[3, 4, 5, 6, 7, 8, 9, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[2, 3, 4, 5, 6, 7, 8, 9, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -3, -4, -5, -6, -7, -8, -9, -10]
-
-[-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -4, -5, -6, -7, -8, -9, -10]
-
-[-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -5, -6, -7, -8, -9, -10]
-
-[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -6, -7, -8, -9, -10]
-
-[-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -7, -8, -9, -10]
-
-[-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -8, -9, -10]
-
-[-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -9, -10]
-
-[-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -10]
-
-[-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
-
-Number of comparisons = 209
-'''
