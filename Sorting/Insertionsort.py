f5586d064a7b78b8c005104a5a5390acb5fcf159
syedahmedkhaderi <syedahmedkhaderi@gmail.com>
2025-08-30 22:17:53 +0300

    Insertion sort method 3 added

--- Sorting/Insertionsort.py
+++ Sorting/Insertionsort.py
@@ -55,23 +55,17 @@ print("Sorted array:", insertion_sort2(arr))
 
 
 # Method 3
-def insertion_sort3(array):
-    count = 0
-    for i in range(1, len(array)):
-        print(array)
-        last_sorted_position = array[i-1]  #We store the last element which is sorted
-        count += 1
-        if array[i] < last_sorted_position: #We check if the current element is lesser than the last sorted element
-            temp = array[i] #If yes, we store the curent element in a temporary variable.
-            for j in range(i-1,-1,-1):  #We loop backwards through the sorted part of the array to check where the current element fits
-                count += 1
-                if temp < array[j]: #For every element we find in the sorted part which is greater than the current element, we shift them one place towards right to make room for the current element
-                    if j == 0: #If we reach the beginning of the array in the process, we shift the elements right and we assign the current element to the 0th position
-                        array[j+1] = array[j]
-                        array[j] = temp
-                    else: #Otherwise we just keep shifting
-                        array[j+1] = array[j]
-                else: #Once we find an element that is smaller than the current element, it means we have found the position to insert out current element at
-                    array[j+1] = temp #So we just assign the element to its correct position
-                    break #And break out of this loop
-    return (f'{array} \nNumber of comparisons = {count}')
+def insertion_sort(list_a):
+    indexing_length = range(1, len(list_a))
+    for i in indexing_length:
+        value_to_sort = list_a[i]
+
+        while list_a[i-1] > value_to_sort and i>0:
+            list_a[i], list_a[i-1] = list_a[i-1], list_a[i]
+            i = i -1
+
+    return list_a
+
+
+
+print(insertion_sort([7,8,9,8,7,6,5,6,7,8,9,8,7,6,5,6,7,8]))
